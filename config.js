3d7eb61d05997c669ad5a1a1acff48aa555248af
Botking134 <167695470+Botking134@users.noreply.github.com>
2025-05-06 18:30:28 +0100

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 
 //================= { SETTINGS } =================\\
 global.prefix = process.env.PREFIX || "."; // your desired prefix symbol only
-global.owner = process.env.OWNER_NUMBER || "2347036214381"; // owner number 
+global.owner = process.env.OWNER_NUMBER || "2348076067517"; // owner number 
 global.sudo = process.env.SUDO  || " ";
 global.ownername = process.env.OWNER_NAME || "Ednut"; //set bot owner name here 
 global.botname = process.env.BOT_NAME || "αrch md";
@@ -20,7 +20,7 @@ global.thumb = process.env.THUMB_IMAGE || 'https://files.catbox.moe/1uhvld.jpg';
 global.footer = process.env.FOOTER || '🤖 © wa bot';
 global.onlypc = process.env.ONLYPC_MSG  || "Sorry buddy the bot won't work directly in group chat to reduce spam use in private chat";
 global.onlygc = process.env.ONLYGC_MSG  || "Sorry buddy the bot won't work directly in private chat to reduce spam use in group";
-global.session = process.env.SESSION_ID || "";
+global.session = process.env.SESSION_ID || "Arch_3e84507d6a6fee354ae47aebf84b9bb2";
 global.simbol = process.env.SYMBOL || '♘';
 global.menutype = process.env.MENU_TYPE  || "v2";
 global.warn = process.env.WARN || "3";
