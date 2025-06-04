b27ca4d1cf2a988a278a65a3425f6b77d452075f
Botking134 <167695470+Botking134@users.noreply.github.com>
2025-06-04 11:18:09 +0100

    Update config.js

--- config.js
+++ config.js
@@ -10,24 +10,24 @@ require('dotenv').config();
 
 //================= { SETTINGS } =================\\
 global.prefix = process.env.PREFIX || ""; // your desired prefix symbol,emoji, leave blank for no prefix
-global.owner = process.env.OWNER_NUMBER || "2347036214381"; // owner number 
-global.sudo = process.env.SUDO  || " ";
-global.ownername = process.env.OWNER_NAME || "Ednut"; //set bot owner name here 
-global.botname = process.env.BOT_NAME || "αrch md";
-global.author = process.env.AUTHOR  || "Ednut";
-global.packname = process.env.PACK_NAME  || "Arch Md ²⁵";
+global.owner = process.env.OWNER_NUMBER || "2347040401291"; // owner number 
+global.sudo = process.env.SUDO  || "2347059092107";
+global.ownername = process.env.OWNER_NAME || "༒𝘖𝘱𝘵𝘪𝘮𝘶𝘴 𝘗𝘳𝘪𝘮𝘦༒"; //set bot owner name here 
+global.botname = process.env.BOT_NAME || "CYBERTRON";
+global.author = process.env.AUTHOR  || "OPTIMUS";
+global.packname = process.env.PACK_NAME  || "PRIME";
 global.thumb = process.env.THUMB_IMAGE || 'https://files.catbox.moe/1uhvld.jpg';
 global.footer = process.env.FOOTER || '🤖 © wa bot';
 global.onlypc = process.env.ONLYPC_MSG  || "Sorry buddy the bot won't work directly in group chat to reduce spam use in private chat";
 global.onlygc = process.env.ONLYGC_MSG  || "Sorry buddy the bot won't work directly in private chat to reduce spam use in group";
-global.session = process.env.SESSION_ID || "";
+global.session = process.env.SESSION_ID || "Arch_2119286348d5d74365adb55b24e2cfce";
 global.simbol = process.env.SYMBOL || '♘';
 global.menutype = process.env.MENU_TYPE  || "v2";
 global.warn = process.env.WARN || "3";
 global.goodbye = process.env.GOODBYE === 'false';
 global.welcome = process.env.WELCOME === 'false';
 global.timezone = process.env.TIME_ZONE || "Africa/Lagos";
-global.startup = process.env.START_MSG === 'false';
+global.startup = process.env.START_MSG === 'true';
 global.scan = 'https://arch-session.onrender.com';
 global.api = '';
 
